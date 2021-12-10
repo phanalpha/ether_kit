c097f7e85547853180b8083aed685e687cf89be5
tsai phan <phanalpha@hotmail.com>
2021-12-11 01:30:43 +0800

    gas price option, the.

--- contract.py
+++ contract.py
@@ -10,11 +10,13 @@ def cli():
 
 
 @cli.command()
+@click.option('--gas', type=int)
+@click.option('--gas-price', type=int, default=None)
 @click.argument('private_key')
 @click.argument('abi', type=click.File())
 @click.argument('bytecode', type=click.File())
 @click.argument('args', nargs=-1)
-def deploy(abi, bytecode, private_key, args):
+def deploy(abi, bytecode, private_key, gas, gas_price, args):
     w3 = Web3()
     contract = w3.eth.contract(
         abi=abi.read(),
@@ -22,7 +24,7 @@ def deploy(abi, bytecode, private_key, args):
     account = w3.eth.account.from_key(private_key)
 
     cc = contract.constructor(*map(eval, args))
-    tx = build_transaction(cc, account, w3)
+    tx = build_transaction(cc, account, w3, gas, gas_price)
     signed = w3.eth.account.sign_transaction(tx, account.key)
     txn = w3.eth.send_raw_transaction(signed.rawTransaction)
     print(w3.toHex(txn))
@@ -31,12 +33,13 @@ def deploy(abi, bytecode, private_key, args):
 @cli.command()
 @click.option('--private-key')
 @click.option('--gas', type=int)
+@click.option('--gas-price', type=int, default=None)
 @click.option('--value', type=int)
 @click.argument('address')
 @click.argument('abi', type=click.File())
 @click.argument('method')
 @click.argument('args', nargs=-1)
-def invoke(private_key, gas, value, address, abi, method, args):
+def invoke(private_key, gas, gas_price, value, address, abi, method, args):
     w3 = Web3()
     contract = w3.eth.contract(address, abi=abi.read())
     fn = contract.functions[method](*map(eval, args))
@@ -46,17 +49,17 @@ def invoke(private_key, gas, value, address, abi, method, args):
         return
 
     account = w3.eth.account.from_key(private_key)
-    tx = build_transaction(fn, account, w3, gas, value)
+    tx = build_transaction(fn, account, w3, gas, gas_price, value)
     signed = w3.eth.account.sign_transaction(tx, account.key)
     txn = w3.eth.send_raw_transaction(signed.rawTransaction)
     print(w3.toHex(txn))
 
 
-def build_transaction(fn, account, web3, gas=None, value=None):
+def build_transaction(fn, account, web3, gas=None, gas_price=None, value=None):
     return fn.buildTransaction({
         **({} if value is None else {'value': value}),
-        'gasPrice': web3.eth.gas_price,
-        'gas': gas or int(1.05 * fn.estimateGas()),
+        'gas': gas or fn.estimateGas(),
+        'gasPrice': gas_price or web3.eth.gas_price,
         'nonce': web3.eth.get_transaction_count(account.address),
     })
 
